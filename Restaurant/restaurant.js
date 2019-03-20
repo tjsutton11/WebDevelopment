87ef6a6dd545959bcc57f1bff91e773a49484d73
tjsutton11 <46410683+tjsutton11@users.noreply.github.com>
2019-03-20 16:35:06 -0400

    finale

---
+++ Restaurant/restaurant.js
@@ -0,0 +1,35 @@
+function clearErrors() {
+    for(var loopCounter = 0; loopCounter < document.forms["contactUs"].elements.length; loopCounter++){
+        if(document.forms["contactUs"].elements[loopCounter].parentElements.className.indexOf("has-") != -1) {
+            document.forms["contactUs"].elements[loopCounter].parentElements.className = "form-group";
+        }
+    }
+}
+
+function validateItems() {
+    clearErrors();
+    var name = document.forms["contactUs"]["name"].value;
+    var email = document.forms["contactUs"]["email"].value;
+    var phone = document.forms["contactUs"]["phone"].value;
+    if(name == ""){
+        alert("Name is required.");
+        document.forms["contactUs"]["name"]
+            .parentElement.className = "form-group has-error";
+        document.forms["contactUs"]["name"].focus();
+        return false;
+    }
+    if(email == ""){
+        alert("Email is required.");
+        document.forms["contactUs"]["email"]
+            .parentElement.className = "form-group has-error";
+        document.forms["contactUs"]["email"].focus();
+        return false;
+    }
+    if(phone == "" || isNaN(phone)){
+        alert("A valid phone number is required.");
+        document.forms["contactUs"]["phone"]
+            .parentElement.className = "form-group has-error";
+        document.forms["contactUs"]["phone"].focus();
+        return false;
+    }
+}
